d622ded3d7c1db5697db1c01198cfe52afa7454a
Maxim <maximplamadeala13@gmail.com>
2022-05-26 00:28:55 +0300

    eu am facut animatia la click dar nu am finisat

---
+++ script.js
@@ -0,0 +1,189 @@
+addEventListener("keydown", e => console.log(e))
+// addEventListener("keyup", e => console.log(e.code))
+
+
+const a1 = document.querySelector(".a1")
+const a2 = document.querySelector(".a2")
+const a3 = document.querySelector(".a3")
+const a4 = document.querySelector(".a4")
+const a5 = document.querySelector(".a5")
+const a6 = document.querySelector(".a6")
+const a7 = document.querySelector(".a7")
+const a8 = document.querySelector(".a8")
+const a9 = document.querySelector(".a9")
+const a0 = document.querySelector(".a0")
+const w = document.querySelector(".W")
+const e = document.querySelector(".E")
+const r = document.querySelector(".R")
+const t = document.querySelector(".T")
+const y = document.querySelector(".Y")
+const u = document.querySelector(".U")
+const i = document.querySelector(".I")
+const o = document.querySelector(".O")
+const p = document.querySelector(".P")
+const a = document.querySelector(".A")
+const s = document.querySelector(".S")
+const d = document.querySelector(".D")
+const f = document.querySelector(".F")
+const g = document.querySelector(".G")
+const h = document.querySelector(".H")
+const j = document.querySelector(".J")
+const k = document.querySelector(".K")
+const l = document.querySelector(".L")
+const z = document.querySelector(".Z")
+const x = document.querySelector(".X")
+const c = document.querySelector(".C")
+const v = document.querySelector(".V")
+const b = document.querySelector(".B")
+const n = document.querySelector(".N")
+const m = document.querySelector(".M")
+const q = document.querySelector(".Q")
+const esc        = document.querySelector(".esc  ")
+const minus      = document.querySelector(".minus")
+const plus       = document.querySelector(".plus ")
+const back       = document.querySelector(".back ")
+const tAB        = document.querySelector(".tAB  ")
+const deschid    = document.querySelector(".deschid")
+const inchid     = document.querySelector(".inchid")
+const bara       = document.querySelector(".bara")
+const cAPS       = document.querySelector(".cAPS ")
+const punctSV    = document.querySelector(".punctSV")
+const GLM        = document.querySelector(".GLM  ")
+const eNTER      = document.querySelector(".eNTER")
+const sHEFT      = document.querySelector(".sHEFT")
+const sHIFT      = document.querySelector(".sHIFT")
+const douaP      = document.querySelector(".douaP    ")
+const ghilemele  = document.querySelector(".ghilemele")
+const punct      = document.querySelector(".punct    ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+addEventListener("keydown", ev => {
+    if (ev.keyCode === 65) {
+        a.classList.toggle("red")
+        a.classList.toggle("apasareRED")
+    } else if (ev.keyCode === 66) {
+        b.classList.toggle("red")
+        b.classList.toggle("apasareRED")
+    } else if (ev.keyCode === 67) {
+        c.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 68) {
+        d.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 69) {
+        e.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 70) {
+        f.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 71) {
+        g.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 72) {
+        h.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 73) {
+        i.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 74) {
+        j.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 75) {
+        k.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 76) {
+        l.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 77) {
+        m.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 78) {
+        n.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 79) {
+        o.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 80) {
+        p.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 81) {
+        q.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 82) {
+        r.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 83) {
+        s.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 84) {
+        t.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 85) {
+        u.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 86) {
+        v.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 87) {
+        w.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 88) {
+        x.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 89) {
+        y.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 90) {
+        z.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 48) {
+        a0.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 49) {
+        a1.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 50) {
+        a2.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 51) {
+        a3.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 52) {
+        a4.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 53) {
+        a5.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 54) {
+        a6.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 55) {
+        a7.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 56) {
+        a8.style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 57) {
+        a9.style.backgroundColor = "#560202"
+    } else if (ev.keyCode === 27) {
+        esc        .style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 8) {
+        back       .style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 9) {
+        tAB        .style.backgroundColor = "#560202"
+    }else if (ev.keyCode === 13) {
+        eNTER      .style.backgroundColor = "#560202"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+addEventListener("keyup", ev => {
+    if (ev.keyCode === 65) {
+        a.classList.toggle("red")
+        a.classList.toggle("apasareRED")
+    }
+})
